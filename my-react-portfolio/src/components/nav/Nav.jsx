3913e1f0ba76f4411a4523deed7993f48b41c6d5
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-18 02:22:10 +0930

    - change scroll down position - add navBar components and their icons

--- my-react-portfolio/src/components/nav/Nav.jsx
+++ my-react-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {MdWorkOutline} from 'react-icons/md'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageMinus} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><MdWorkOutline /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#contact"><BiMessageMinus /></a>
+    </nav>
   )
 }
 
