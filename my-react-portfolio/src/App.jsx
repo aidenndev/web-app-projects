b005435cec5777e7eae2ee0977b01c6334a2dbbf
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-17 23:53:12 +0930

    - create innitial default css - test app working

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div><Header/></div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
