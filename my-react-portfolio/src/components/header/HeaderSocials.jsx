e66b0996417b9a6f1fa47fb982ffa14fe8aa2d77
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-18 01:52:03 +0930

    add profile pic css

--- my-react-portfolio/src/components/header/HeaderSocials.jsx
+++ my-react-portfolio/src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import {AiFillGithub} from 'react-icons/ai'
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
+    <div className='header_socials'>
         <a href="https://www.linkedin.com/in/aidendeveloper/" target='_blank'><AiFillLinkedin /></a>
         <a href="https://github.com/aidenndev" target='_blank'>
             <AiFillGithub />
