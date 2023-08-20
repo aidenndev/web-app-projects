f1c0c052f7d14243998d8a5701a6ab5066e6b3e7
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-21 00:57:29 +0930

    added portfolio components & css

--- my-react-portfolio/src/components/nav/Nav.jsx
+++ my-react-portfolio/src/components/nav/Nav.jsx
@@ -6,6 +6,7 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {MdWorkOutline} from 'react-icons/md'
 import {RiServiceLine} from 'react-icons/ri'
+import {AiFillProject} from 'react-icons/ai'
 import {BiMessageMinus} from 'react-icons/bi'
 
 const Nav = () => {
@@ -16,6 +17,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdWorkOutline /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#fortfolio' ? 'active' : ''}><AiFillProject /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageMinus /></a>
     </nav>
   )
--- my-react-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-react-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio-1.png'
+import IMG2 from '../../assets/portfolio-2.png'
+import IMG3 from '../../assets/portfolio-3.png'
+import IMG4 from '../../assets/portfolio-4.png'
+import IMG5 from '../../assets/portfolio-5.png'
+import IMG6 from '../../assets/portfolio-6.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className='portfolio_item'>
+          <div className='portfolio_item-img'>
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Title</h3>
+          <div className='portfolio_item-cta'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-img'>
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Title</h3>
+          <div className='portfolio_item-cta'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-img'>
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>Title</h3>
+          <div className='portfolio_item-cta'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-img'>
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>Title</h3>
+          <div className='portfolio_item-cta'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-img'>
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>Title</h3>
+          <div className='portfolio_item-cta'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-img'>
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>Title</h3>
+          <div className='portfolio_item-cta'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+
+
+      </div>
+    </section>
   )
 }
 
