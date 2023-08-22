5e411c6ad0b81b1b61a1bf45f1873dd64ecef2b9
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2023-08-22 21:31:44 +0930

    added initial testinominal components

--- my-react-portfolio/src/components/testimonials/Testimonials.jsx
+++ my-react-portfolio/src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,18 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>What do my clients think?</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+        <article className='testinomial'>
+          <div className="client_avatar">
+            <img src="" alt="" />
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
