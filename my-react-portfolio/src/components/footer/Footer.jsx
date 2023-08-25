03547fda993d608b53a389f3411990fd83e5c584
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-25 22:50:28 +0930

    added footer and released the beta

--- my-react-portfolio/src/components/footer/Footer.jsx
+++ my-react-portfolio/src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>AIDEN NGUYEN</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+      <a href="https://www.linkedin.com/in/aidendeveloper/" target='_blank'><AiFillLinkedin /></a>
+        <a href="https://github.com/aidenndev" target='_blank'>
+            <AiFillGithub />
+        </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Aiden Nguyen. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
