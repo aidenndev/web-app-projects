24c23c00cee593aea666bb5d53a0e8783b735d26
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-28 01:10:11 +0930

    added About components

---
+++ my-react-ai-portfolio/src/components/about/subheadingData.jsx
@@ -0,0 +1,75 @@
+const subheadingData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+          </p>
+        </>
+      )
+    }
+  ],
+
+  2: [
+    {
+      title: "2023",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+          </p>
+        </>
+      )
+    },
+
+    {
+      title: "2020",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+          </p>
+        </>
+      )
+    },
+
+    {
+      title: "2018",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+          </p>
+        </>
+      )
+    }
+  ],
+
+  3: [
+    {
+      title: "2022 Software Developer",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+          </p>
+        </>
+      )
+    },
+
+    {
+      title: "2020 Front End Developer",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+          </p>
+        </>
+      )
+    },
+  ],
+}
+
+export default subheadingData
