7fd01aad9eb5ac6a1c2ab8b057e95dd753904dc8
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-28 02:41:26 +0930

    added skills & css

---
+++ my-react-ai-portfolio/src/components/skills/skillsData.jsx
@@ -0,0 +1,65 @@
+const skills = {
+    1: [
+        {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "CSS",
+            level: 5,
+        },
+        {
+            title: "JavaScript",
+            level: 4,
+        },
+        {
+            title: "TypeScript",
+            level: 1,
+        },
+        {
+            title: "React JS",
+            level: 4,
+        },
+        {
+            title: "Ruby on Rails",
+            level: 2,
+        },
+    ],
+
+    2: [
+        {
+            title: "Node JS",
+            level: 5,
+        },
+        {
+            title: "ASP.NET MVC",
+            level: 4,
+        },
+        {
+            title: "ASP.NET",
+            level: 5,
+        },
+        {
+            title: "git",
+            level: 4,
+        },
+        {
+            title: "MySQL",
+            level: 3,
+        },
+        {
+            title: "PostgreSQL",
+            level: 2,
+        },
+        {
+            title: "Ruby",
+            level: 3,
+        },
+        {
+            title: "Swift",
+            level: 4,
+        },
+    ]
+}
+
+export default skills
