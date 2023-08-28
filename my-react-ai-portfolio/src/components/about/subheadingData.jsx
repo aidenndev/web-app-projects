b9c2681ef2c523cf707b8fd235459b7d3db148c2
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-29 00:26:05 +0930

    changed some css files

--- my-react-ai-portfolio/src/components/about/subheadingData.jsx
+++ my-react-ai-portfolio/src/components/about/subheadingData.jsx
@@ -4,13 +4,13 @@ const subheadingData = {
       title: "About Me",
       content: (
         <>
-          <p>
-            Hello! I'm Aiden.
-            I'm developing Creative and Interactive webapps.
-          </p>
-          <p>
-            As a frontend developer, my expertise lies in creating seamless and intuitive user experiences. I specilize in using cutting-edge technologies and graphic design skills to produce high quality and creative web applications. Specifically, I have advanced knowledge of ReactJS and NodeJS allowing me to build complex and dynamic applications.
-          </p>
+            <p>
+              Hello! I'm Aiden.
+              I'm developing Creative and Interactive webapps.
+            </p>
+            <p>
+              As a frontend developer, my expertise lies in creating seamless and intuitive user experiences. I specilize in using cutting-edge technologies and graphic design skills to produce high quality and creative web applications. Specifically, I have advanced knowledge of ReactJS and NodeJS allowing me to build complex and dynamic applications.
+            </p>
         </>
       )
     }
@@ -20,80 +20,80 @@ const subheadingData = {
     {
       title: "2023",
       content: (
-          <>
-            <p>
+        <>
+          <p>
             Professional Year Program Certificate
-            </p>
-            <p>
+          </p>
+          <p>
             at Performance Education
-            </p>
-          </>
-        )
-      },
-  
-{
-  title: "2020",
+          </p>
+        </>
+      )
+    },
+
+    {
+      title: "2020",
       content: (
-  <>
-    <p>
-      Bachelor of Information Technology (Software Development)
-    </p>
-    <p>
-      at University of South Australia
-    </p>
-  </>
-        )
-      },
+        <>
+          <p>
+            Bachelor of Information Technology (Software Development)
+          </p>
+          <p>
+            at University of South Australia
+          </p>
+        </>
+      )
+    },
 
-{
-  title: "2018",
-    content: (
-      <>
-        <p>
-          Diploma of Information Technology
-        </p>
-        <p>
-          at South Australian Institute of Business & Technology
-            </p>
-          </>
-              )
-      }
-    ],
-              
-    3: [
-            {
-              title: "2023 Frontend Developer",
-            content: (
-            <>
-              <p>
-                Freelancer
-              </p>
-            </>
-            )
-      },
-            {
-          title: "2022 Software Developer",
-          content: (
-      <>
-        <p>
+    {
+      title: "2018",
+      content: (
+        <>
+          <p>
+            Diploma of Information Technology
+          </p>
+          <p>
+            at South Australian Institute of Business & Technology
+          </p>
+        </>
+      )
+    }
+  ],
+
+  3: [
+    {
+      title: "2023 Frontend Developer",
+      content: (
+        <>
+          <p>
+            Freelancer
+          </p>
+        </>
+      )
+    },
+    {
+      title: "2022 Software Developer",
+      content: (
+        <>
+          <p>
             Intern at GaP Solutions (South Australia)
-            </p >
-          </>
-        )
-},
+          </p >
+        </>
+      )
+    },
 
-{
-  title: "2020 Front End Developer",
-    content: (
-      <>
-        <p>
-          at Vietan Logistics (Vietnam)
-          and Claymore Wines (South Australia)
-        </p>
-      </>
-    )
+    {
+      title: "2020 Front End Developer",
+      content: (
+        <>
+          <p>
+            at Vietan Logistics (Vietnam)
+            and Claymore Wines (South Australia)
+          </p>
+        </>
+      )
+    }
+  ]
 }
-    ]
-  }
 
 export default subheadingData
