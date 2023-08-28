187ddf80f767e264ee0257ad07798a87b9c56ba4
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-28 23:26:25 +0930

    added Contact components with EmailJS

--- my-react-ai-portfolio/src/components/about/subheadingData.jsx
+++ my-react-ai-portfolio/src/components/about/subheadingData.jsx
@@ -1,75 +1,99 @@
 const subheadingData = {
-    1: [
-      {
-        title: "About Me",
-        content: (
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Hello! I'm Aiden.
+            I'm developing Creative and Interactive webapps.
+          </p>
+          <p>
+            As a frontend developer, my expertise lies in creating seamless and intuitive user experiences. I specilize in using cutting-edge technologies and graphic design skills to produce high quality and creative web applications. Specifically, I have advanced knowledge of ReactJS and NodeJS allowing me to build complex and dynamic applications.
+          </p>
+        </>
+      )
+    }
+  ],
+
+  2: [
+    {
+      title: "2023",
+      content: (
           <>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+            Professional Year Program Certificate
             </p>
-          </>
-        )
-      }
-    ],
-  
-    2: [
-      {
-        title: "2023",
-        content: (
-          <>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
-            </p>
-          </>
-        )
-      },
-  
-      {
-        title: "2020",
-        content: (
-          <>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+            at Performance Education
             </p>
           </>
         )
       },
   
-      {
-        title: "2018",
-        content: (
-          <>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
+{
+  title: "2020",
+      content: (
+  <>
+    <p>
+      Bachelor of Information Technology (Software Development)
+    </p>
+    <p>
+      at University of South Australia
+    </p>
+  </>
+        )
+      },
+
+{
+  title: "2018",
+    content: (
+      <>
+        <p>
+          Diploma of Information Technology
+        </p>
+        <p>
+          at South Australian Institute of Business & Technology
             </p>
           </>
-        )
+              )
       }
     ],
-  
+              
     3: [
-      {
-        title: "2022 Software Developer",
-        content: (
-          <>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
-            </p>
+            {
+              title: "2023 Frontend Developer",
+            content: (
+            <>
+              <p>
+                Freelancer
+              </p>
+            </>
+            )
+      },
+            {
+          title: "2022 Software Developer",
+          content: (
+      <>
+        <p>
+            Intern at GaP Solutions (South Australia)
+            </p >
           </>
         )
-      },
-  
-      {
-        title: "2020 Front End Developer",
-        content: (
-          <>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi provident deserunt voluptate, quod vel cum animi repellendus, facere ea temporibus repudiandae libero sint pariatur hic eius. Officiis excepturi nobis natus.
-            </p>
-          </>
-        )
-      },
-    ],
+},
+
+{
+  title: "2020 Front End Developer",
+    content: (
+      <>
+        <p>
+          at Vietan Logistics (Vietnam)
+          and Claymore Wines (South Australia)
+        </p>
+      </>
+    )
+}
+    ]
   }
-  
-  export default subheadingData
+
+export default subheadingData
