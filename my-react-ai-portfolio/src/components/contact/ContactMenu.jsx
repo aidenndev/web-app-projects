8c0367a97a33dbe936f871a5aa8bc6fe20859ca1
aidenndev <aiden.nguyen.dev@gmail.com>
2023-08-28 22:35:29 +0930

    added dompurify for sending email security

---
+++ my-react-ai-portfolio/src/components/contact/ContactMenu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import emailjs from 'emailjs-com'
+import DOMPurify from 'dompurify'
+
+export default class ContactMenu extends Component {
+  render() {
+    return (
+      <div>ContactMenu</div>
+    )
+  }
+}
