391c73cdde00fced7b78368da8c37bffeb09bb5f
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2023-08-31 01:50:05 +0930

    added Navbar

---
+++ react-typescript-shopping-cart/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as BsNavBar } from 'react-bootstrap'
+
+export function NavBar() {
+    return (
+        <BsNavBar className='bg-white shadow-sm mb-3'>
+            <Container>Nav</Container>
+        </BsNavBar>
+    )
+}
