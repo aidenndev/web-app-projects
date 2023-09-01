05ea1bb435b8f079a1173f28946285ecb11e8bc6
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2023-09-01 21:52:08 +0930

    added subHeading of the header

--- aiden-restaurant/src/container/Header/Header.jsx
+++ aiden-restaurant/src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
+      <SubHeading title="Welcome to Aiden Restaurant" />
       <h1 className="app__header-h1">Taste the Tradition</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>We carry the pride of the traditional flavour - bringing with it the quintessential recipes that has been perfected for more than 30 years.</p>
       <button type="button" className="custom__button">Explore Menu</button>
