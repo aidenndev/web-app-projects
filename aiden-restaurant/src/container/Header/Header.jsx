b0b23643acb35aff1b479a8cf75ea6c976a180f6
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2023-09-01 21:46:56 +0930

    added Header components and css
    added some small transition aspects to the index css file

--- aiden-restaurant/src/container/Header/Header.jsx
+++ aiden-restaurant/src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">Taste the Tradition</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>We carry the pride of the traditional flavour - bringing with it the quintessential recipes that has been perfected for more than 30 years.</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 
