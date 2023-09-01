3e91d779a869bd5d0f148611e00dec9aadbc6217
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2023-09-01 22:25:46 +0930

    added about us components & css

--- aiden-restaurant/src/container/AboutUs/AboutUs.jsx
+++ aiden-restaurant/src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">The characteristics of Vietnamese culture are also reproduced through the design of Aiden Restaurant's space, which is inspired by the traditional Vietnamese families. Immerse yourself in the journey of discovering Vietnamese identity with us.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">We believe that Vietnamese culture is more than just a sandwich or an ao dai. It also converges in the family element, where every dinner becomes a precious opportunity for all of us to get together. On the cozy dining table, stories are shared, laughter is heard and memories come from there.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+    </div>
   </div>
 );
 
