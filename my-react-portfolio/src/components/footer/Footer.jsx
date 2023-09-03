fb6327f31afa1ea1bf2387feda639a24acaa6b60
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2023-09-04 01:31:32 +0930

    fixed footer and recolored

--- my-react-portfolio/src/components/footer/Footer.jsx
+++ my-react-portfolio/src/components/footer/Footer.jsx
@@ -1,32 +1,29 @@
 import React from 'react'
 import './footer.css'
-import {AiFillLinkedin} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
+import {AiFillInstagram} from 'react-icons/ai'
+import {AiOutlineWhatsApp} from 'react-icons/ai'
+import {FaViber} from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer_logo'>AIDEN NGUYEN</a>
+      <a href="#" className='footer_logo'>JAYDEN NGUYEN</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#">Trang chủ</a></li>
+        <li><a href="#about">Giới thiệu</a></li>
+        <li><a href="#portfolio">Hành trình </a></li>
+        <li><a href="#contact">Liên hệ</a></li>
       </ul>
 
       <div className="footer_socials">
-      <a href="https://www.linkedin.com/in/aidendeveloper/" target='_blank'><AiFillLinkedin /></a>
-        <a href="https://github.com/aidenndev" target='_blank'>
-            <AiFillGithub />
-        </a>
+        <a href="https://www.instagram.com/iamc92/" target='_blank'><AiFillInstagram /></a>
+        <a href="viber://chat?number=+84909081701" target='_blank'><FaViber /></a>
+        <a href="http://api.whatsapp.com/send?phone=+840909081701" target='_blank'><AiOutlineWhatsApp /></a>
       </div>
 
       <div className="footer_copyright">
-        <small>&copy; Aiden Nguyen. All rights reserved.</small>
+        <small>&copy; Jayden Nguyen. All rights reserved.</small>
       </div>
     </footer>
   )
